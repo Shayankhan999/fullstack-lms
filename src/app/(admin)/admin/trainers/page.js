0e4f3bb4c9eb6527f4e2ba006c440c1a2e295133
Shayankhan <shayankhan9148@gmail.com>
2024-11-17 12:57:48 +0500

    added successfully

--- src/app/(admin)/admin/trainers/page.js
+++ src/app/(admin)/admin/trainers/page.js
@@ -6,7 +6,7 @@ export default function Trainers(){
     return(
         <div className="min-h-screen p-10">
              <div className="flex justify-between my-2">
-            <h1 className="text-3xl font-bold text-center">Trainer</h1>
+            <h1 className="text-3xl font-bold text-center">Trainersss</h1>
                 <TrainerModal/>
             </div>
             <TrainersTable/>
